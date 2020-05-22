7ec91f6840fc7ff3d3a9cb51ff1745d472e698fc
v_tianzhiyuan <v_tianzhiyuan@baidu.com>
2020-05-22 18:52:47 +0800

    Initial commit

---
+++ src/main/java/com/example/fivespace/HeapSpaceTest.java
@@ -0,0 +1,4 @@
+package com.example.fivespace;
+
+public class HeapSpaceTest {
+}
---
+++ src/main/java/com/example/fivespace/StackSpaceTest.java
@@ -0,0 +1,11 @@
+package com.example.fivespace;
+
+/***
+ * 栈 测试类
+ */
+public class StackSpaceTest {
+
+    public static void main(String[] args) {
+
+    }
+}
